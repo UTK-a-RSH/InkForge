affb84d1c772093f57547c8b0d9ad4cc93433b1e
UtkarshLB216 <utkarsh2212gonda@gmail.com>
2024-07-14 04:40:53 +0530

    create signup api route

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 
@@ -12,9 +13,11 @@ mongoose.connect(process.env.MONGO_DB_URI).then(() => {
 })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoutes);
