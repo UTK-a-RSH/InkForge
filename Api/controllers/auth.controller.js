6cda42efa62a953ed417c702862b707f58b26c99
UtkarshLB216 <utkarsh2212gonda@gmail.com>
2024-07-20 18:46:30 +0530

    complete signin page functionality

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -3,29 +3,30 @@ import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
 
-export const signup = async(req, res, next) => {
-    const {email,  password} = req.body;
+export const signup = async(req, res) => {
+    const {username, email, password} = req.body;
 
-    if(!email || !email || !password || email === '' || email === '' || password === ''){
-        next(errorHandler(400, 'All fields are required'));
+    if(!username || !email || !password || username === '' || email === '' || password === ''){
+        return res.status(400).json({message : 'All fields are required'});
     }
-    
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const user = new User({
-        email,
+    const newUser = new User({
+        username,
         email,
         password : hashedPassword,
     });
 
     try {
-        await user.save();
+        await newUser.save();
     res.json('Signup successful');
     }
     catch (error) {
         next(error);
     }
 };
+
 
 export const signin = async(req, res, next) => {
     const {email, password} = req.body;
